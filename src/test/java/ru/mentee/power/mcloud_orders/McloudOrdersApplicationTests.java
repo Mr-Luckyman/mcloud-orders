5d8ff39c65b20c44f09f9b124812a16a104361f3
Stas <shadrin060388@gmail.com>
2026-06-27 17:18:57 +0300

    fix: исправлен тест для CI профиля

--- src/test/java/ru/mentee/power/mcloud_orders/McloudOrdersApplicationTests.java
+++ src/test/java/ru/mentee/power/mcloud_orders/McloudOrdersApplicationTests.java
@@ -2,8 +2,10 @@ package ru.mentee.power.mcloud_orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class McloudOrdersApplicationTests {
 
 	@Test
