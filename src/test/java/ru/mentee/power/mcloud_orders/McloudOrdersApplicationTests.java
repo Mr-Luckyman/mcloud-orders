eb52f09b4109e086737d8531908a3706655af48c
Stas <shadrin060388@gmail.com>
2026-06-27 17:22:49 +0300

    fix: исправлен CI профиль и тесты

--- src/test/java/ru/mentee/power/mcloud_orders/McloudOrdersApplicationTests.java
+++ src/test/java/ru/mentee/power/mcloud_orders/McloudOrdersApplicationTests.java
@@ -3,6 +3,8 @@ package ru.mentee.power.mcloud_orders;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("ci")
@@ -12,4 +14,8 @@ class McloudOrdersApplicationTests {
 	void contextLoads() {
 	}
 
+	@DynamicPropertySource
+	static void kafkaProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.kafka.bootstrap-servers", () -> "localhost:9092");
+	}
 }
